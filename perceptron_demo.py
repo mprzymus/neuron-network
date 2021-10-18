2e1138d3e4041ecd4b4bf88d2efacdd7c50509af
mprzymus <pzrymus@gmail.com>
2021-10-19 01:06:25 +0200

    file names refactor

--- perceptron_demo.py
+++ perceptron_demo.py
@@ -1,21 +1,8 @@
 import numpy as np
 
+from extension_data import extend_data
 from perceptron import Perceptron
 from activation_function import threshold_bipolar
-
-
-def extend_data(x_train, y_train, factor):
-    array_x = []
-    array_y = []
-    for count, x in enumerate(x_train):
-        array_x.append(x)
-        array_y.append(y_train[count])
-        for i in range(factor):
-            new_data = x + (np.random.rand(np.size(x)) * 0.01)
-            array_x.append(new_data)
-            array_y.append(y_train[count])
-    return np.array(array_x), np.array(array_y)
-
 
 x_train_unipolar = np.array([[1, 1], [1.1, 1], [1, 1.1], [1, 0], [0, 1], [0, 0]])
 y_train_unipolar = np.array([1, 1, 1, 0, 0, 0])
