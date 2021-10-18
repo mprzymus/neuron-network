d7caf4ad0b8227eadfbfbdcd019042a22546f189
mprzymus <pzrymus@gmail.com>
2021-10-18 22:26:18 +0200

    perceptron customization

--- perceptron.py
+++ perceptron.py
@@ -4,11 +4,16 @@ from activation_function import threshold_unipolar
 
 
 class Perceptron:
-    def __init__(self, weights=None, act_function=threshold_unipolar, alfa=0.01, size=2):
+    def __init__(self, weights=None, act_function=threshold_unipolar, alfa=0.01, size=2, bias=None, bias_static=False):
         self.weights = weights
-        self.act_function = act_function
-        self.bias = np.random.rand() * 2 - 1
+        self.bias_static = bias_static
+        self.epochs = 0
         self.alfa = alfa
+        self.act_function = act_function
+        if bias is None:
+            self.bias = np.random.rand() * 2 - 1
+        else:
+            self.bias = bias
         self.size = size
         if self.weights is None:
             self.weights = np.random.rand(size) * 2 - 1
@@ -19,17 +24,20 @@ class Perceptron:
         total = np.sum(x_s) + self.bias
         return self.act_function(total)
 
-    def fit(self, x_train, y_train):
+    def fit(self, x_train, y_train, verbose=False):
         was_error = True
         i = 0
         while was_error:
             was_error = False
-            print(f"epoch {i}")
-            print(f"{self.weights}, {self.bias}")
+            if verbose:
+                print(f"epoch {i}")
+                print(f"{self.weights}, {self.bias}")
             i += 1
             for count, x in enumerate(x_train):
                 y_predict = self.predict(x)
                 delta = y_train[count] - y_predict
                 self.weights += delta * self.alfa * x
-                self.bias += delta * self.alfa
+                if not self.bias_static:
+                    self.bias += delta * self.alfa
                 was_error = was_error if was_error else y_predict != y_train[count]
+        self.epochs = i
