derivatives fixed. Refactoring

l2/layers.py
@@ -28,6 +28,7 @@ class Layer:
             self.bias = bias
         self.last_result = None
         self.last_input = None
+        self.last_act = None
 
     def output_size(self):
         layer_size, _ = self.weights.shape
@@ -38,15 +39,16 @@ class Layer:
         return self.apply_activation()
 
     def apply_activation(self):
-        return np.vectorize(self.act_function.apply)(self.last_result)
+        self.last_act = np.vectorize(self.act_function.apply)(self.last_result)
+        return self.last_act
 
     def calculate_act_input(self, input_vector):
         self.last_input = input_vector
         weighted = self.weights.dot(input_vector)
         self.last_result = self.bias + weighted
 
-    def loss_derivative(self):
-        return np.vectorize(self.act_function.apply_derivative)(self.last_result)
+    def act_derivative(self):
+        return np.vectorize(self.act_function.apply_derivative)(self.last_act)
 
 
 class Softmax(Layer):

l2/network.py
@@ -4,7 +4,7 @@ from l2.activation_function import *
 from l2.extension_data import x_train_unipolar_aug, y_train_unipolar_aug
 from l2.layers import Layer, Softmax, GaussianWeightsInitStrategy
 
-EPOCHS = 10
+EPOCHS = 20
 
 
 def print_if_verbose(verbose, to_print):
@@ -61,7 +61,7 @@ class Network:
                 softmax_loss += np.outer(self.softmax.last_input, total_loss).T
                 epoch_error += total_loss.sum()
                 for count, layer in enumerate(self.layers[::-1]):
-                    derivative = layer.loss_derivative()
+                    derivative = layer.act_derivative()
                     this_layer_loss = next_layer.weights.T.dot(next_layer_loss) * derivative
                     loss_bias[count] += this_layer_loss
                     loss[count] += np.outer(layer.last_input, this_layer_loss).T