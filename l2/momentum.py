a079e8abe4694c3d3734e14bc3b6ffc2550f5b9a
mprzymus <przymus@gmail.com>
2021-11-26 00:47:36 +0100

    momentum algorithms

---
+++ l2/momentum.py
@@ -0,0 +1,60 @@
+class MomentumStrategy:
+    def apply_momentum(self, batch_size, loss, bias_loss, gradient_fun, layer_number):
+        return gradient_fun(batch_size, loss[layer_number]), gradient_fun(batch_size, bias_loss[layer_number])
+
+    def apply_momentum_softmax(self, batch_size, loss, bias_loss, gradient_fun):
+        return gradient_fun(batch_size, loss), gradient_fun(batch_size, bias_loss)
+
+
+class Momentum(MomentumStrategy):
+    def __init__(self, momentum_rate, error_matrix_generator):
+        self.momentum_rate = momentum_rate
+        self.prev_errors = error_matrix_generator.init_loss()
+        self.prev_bias_errors = error_matrix_generator.init_loss_bias()
+        self.prev_softmax_error = error_matrix_generator.init_softmax_loss()
+        self.prev_softmax_bias_error = error_matrix_generator.init_softmax_loss_bias()
+
+    def apply_momentum(self, batch_size, loss, bias_loss, gradient_fun, layer_number):
+        v_weights = self.momentum_rate * self.prev_errors[layer_number]
+        v_bias = self.momentum_rate * self.prev_bias_errors[layer_number]
+        delta_weights = gradient_fun(batch_size, loss[layer_number]) + v_weights
+        delta_bias = gradient_fun(batch_size, bias_loss[layer_number]) + v_bias
+        self.prev_errors[layer_number] = delta_weights
+        self.prev_bias_errors[layer_number] = delta_bias
+        return delta_weights, delta_bias
+
+    def apply_momentum_softmax(self, batch_size, loss, bias_loss, gradient_fun):
+        v_weights = self.momentum_rate * self.prev_softmax_error
+        v_bias = self.momentum_rate * self.prev_softmax_bias_error
+        delta_weights = gradient_fun(batch_size, loss) + v_weights
+        delta_bias = gradient_fun(batch_size, bias_loss) + v_bias
+        self.prev_softmax_error = delta_weights
+        self.prev_softmax_bias_error = delta_bias
+        return delta_weights, delta_bias
+
+
+class Nag(MomentumStrategy):
+    def __init__(self, momentum_rate, error_matrix_generator):
+        self.momentum_rate = momentum_rate
+        self.prev_errors = error_matrix_generator.init_loss()
+        self.prev_bias_errors = error_matrix_generator.init_loss_bias()
+        self.prev_softmax_error = error_matrix_generator.init_softmax_loss()
+        self.prev_softmax_bias_error = error_matrix_generator.init_softmax_loss_bias()
+
+    def apply_momentum(self, batch_size, loss, bias_loss, gradient_fun, layer_number):
+        v_weights = self.momentum_rate * self.prev_errors[layer_number]
+        v_bias = self.momentum_rate * self.prev_bias_errors[layer_number]
+        delta_weights = gradient_fun(batch_size, loss[layer_number] - self.momentum_rate * v_weights) + v_weights
+        delta_bias = gradient_fun(batch_size, bias_loss[layer_number] - self.momentum_rate * v_bias) + v_bias
+        self.prev_errors[layer_number] = delta_weights
+        self.prev_bias_errors[layer_number] = delta_bias
+        return delta_weights, delta_bias
+
+    def apply_momentum_softmax(self, batch_size, loss, bias_loss, gradient_fun):
+        v_weights = self.momentum_rate * self.prev_softmax_error
+        v_bias = self.momentum_rate * self.prev_softmax_bias_error
+        delta_weights = gradient_fun(batch_size, loss - self.momentum_rate * v_weights) + v_weights
+        delta_bias = gradient_fun(batch_size, bias_loss - self.momentum_rate * v_bias) + v_bias
+        self.prev_softmax_error = delta_weights
+        self.prev_softmax_bias_error = delta_bias
+        return delta_weights, delta_bias
